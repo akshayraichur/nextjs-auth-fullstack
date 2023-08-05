5de4e9b29b760d3490e22817125618771d83ff40
akshayraichur <akshay.raichur9@gmail.com>
2023-08-05 15:34:23 +0530

    feat: add protected route, middleware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,46 @@
+"use client";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+
 export default function ProfilePage() {
+  const router = useRouter();
+  const [data, setData] = useState("");
+  const logoutHandler = async () => {
+    try {
+      const response = await axios.get("/api/users/logout");
+      toast.success("Logout done");
+      router.push("/login");
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  };
+
+  const getUserDetails = async () => {
+    const response = await axios.get("/api/users/me");
+    console.log(response.data.user);
+    setData(response.data.user);
+  };
+  console.log(data);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
+      <button onClick={logoutHandler} className="rounded bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4">
+        Logout
+      </button>
+
+      <button
+        onClick={getUserDetails}
+        className="rounded bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4"
+      >
+        Get data
+      </button>
+
+      <div>{/* <h2>{data}</h2> */}</div>
     </div>
   );
 }
